ba14a7e0c6928ce2a615fa684b3cbdb6b5034c14
Isabela Meyer <isabela.demf@gmail.com>
2021-09-21 20:32:51 -0300

    Add routes to token and token info in UrlsController. Show and redirect method in UrlsController created. Show page with original url, new tiny url and button to redirect to page created.

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,10 +2,19 @@ class UrlsController < ApplicationController
   def create
     @url = Url.new(url_params)
     if @url.save
-      redirect_to root_path
+      redirect_to url_info_path(@url.token)
     else
       render 'pages/home'
     end
+  end
+
+  def show
+    @url = Url.find_by_token(params[:token])
+  end
+
+  def redirect
+    @url = Url.find_by_token(params[:token])
+    redirect_to token_page_path(@url.token)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :urls, only: :create
+  get  '/:token', to: 'urls#redirect', as: :token_page
+  get  '/:token/info', to: 'urls#show', as: :url_info
 end
