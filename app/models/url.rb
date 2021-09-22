6f1cd27134042b27b85fd05cfdd40218d99a6481
Isabela Meyer <isabela.demf@gmail.com>
2021-09-21 21:12:50 -0300

    Add hash as default to url ips.

--- app/models/url.rb
+++ app/models/url.rb
@@ -4,7 +4,7 @@ class Url < ApplicationRecord
   validates_presence_of :original_url
   validates_uniqueness_of :original_url
 
-  serialize :ips
+  serialize :ips, Hash
 
   def token_generator
     self.token = Token::TokenGenerator.new(original_url).generate_token
