5fa10a8aeb61904aedeb08edecffafd2ba446581
Isabela Meyer <isabela.demf@gmail.com>
2021-09-21 21:13:02 -0300

    Add hash as default to url ips.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_221057) do
+ActiveRecord::Schema.define(version: 2021_09_22_000502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_09_21_221057) do
     t.string "original_url"
     t.string "token"
     t.text "ips"
-    t.integer "visitors_count"
+    t.integer "visitors_count", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
